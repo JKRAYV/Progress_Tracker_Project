12d80de3d2e26f43a65397a6d4f29834ce7eba4b
JKRAYV <joshua.kraayvanger@gmail.com>
2023-08-17 11:24:49 -0500

    Added templates, and routes for login, register, tv, and users.

--- app.py
+++ app.py
@@ -1,44 +1,59 @@
-from flask import Flask, jsonify, request
-import pymongo
+from flask import Flask, jsonify, request, render_template, redirect
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-"""try: 
-    mongo = pymongo.MongoClient(
-        host = "localhost",
-        port = 27017,
-        severlSelectionTimeout = 1000
-    )
-    mongo.server_info() # trigger exception if cannot connect to database 
-except: 
-    print("ERROR- cannot connect to db")"""
+bcrypt = Bcrypt(app)
 
-#mongodata = {"Hello":"World"}
-@app.route('')
-def login(): 
-    return '<h1>TV Tracker<h1>'
+try: 
+    app = Flask(__name__)
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/progress_tracker"
+    mongo = PyMongo(app) 
+except: 
+        print("ERROR- cannot connect to db")
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = mongo.db.Users.find_one({"Username": username})
+
+        if user and Bcrypt.check_password_hash(user["Password"], password):
+            # Successful login
+            return redirect("/dashboard")
+
+        # Invalid credentials
+        error = "Invalid username or password"
+        return render_template("login.html", error=error)
+
+    return render_template("login.html")
 
 @app.route("/users", methods=["GET","POST"])
 def get_users():
     if request.method == "GET":
-        # Handle Mongo queries here
-        data = query.all()
-        # return data to jsonify method
-        return jsonify(mongodata)
+        users = list(mongo.db.Users.find())
+        for user in users:
+            user["_id"] = str(user["_id"])  # Convert ObjectId to string for JSON serialization    
+        return render_template("./users.html", users=users)
 
     elif request.method == "POST":
-        post_data = request.json["key"]
-        print(post_data)
-        # Handle Mongo interactions here
+        post_data = request.json
+        # Assuming post_data is a dictionary containing user details
+        new_user_id = mongo.db.users.insert_one(post_data).inserted_id
+        return jsonify(str(new_user_id)), 201  # Return the new user's ID and HTTP status code 201 (Created)
 
     return "request handled"
 
 @app.route("/tvshows", methods=["GET","POST"])
 def get_tvshows():
     if request.method == "GET":
-        # Handle Mongo queries here
-        # return data to jsonify method
-        return jsonify(mongodata)
+        tv = list(mongo.db.TV.find())
+        for show in tv:
+            show["_id"] = str(show["_id"])  # Convert ObjectId to string for JSON serialization    
+        return render_template("./tv.html", tv=tv)
 
     elif request.method == "POST":
         post_data = request.json["key"]
@@ -47,5 +62,5 @@ def get_tvshows():
 
     return "request handled"
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
